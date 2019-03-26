dd5a83e958ad9f73a0285e465ca4b2a5f9f88292
Awais Ahmad <awais.ahmad1990@yahoo.com>
2019-03-26 16:55:31 +0500

    URL VALIDATION BUG FIXED

--- validation/profile.js
+++ validation/profile.js
@@ -25,7 +25,7 @@ module.exports = function validateProfileInput(data) {
   }
 
   if (!isEmpty(data.website)) {
-    if (!Validator.isURL(data.website)) {
+    if (!Validator.isURL(data.website,[http,https,ftp])) {
       errors.website = 'Not a valid URL';
     }
   }
